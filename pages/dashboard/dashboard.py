a6e6c58c7d4d27236145e21be9db9ce8d204b51c
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2024-02-29 11:04:04 -0500

    update dashboard

--- pages/dashboard/dashboard.py
+++ pages/dashboard/dashboard.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from taipy.gui import Markdown
-import pandas as pd
-import json
+import pandas  as pd
+import plotly.express as px
 
-data = '../data/data.csv'
+data = pd.read_csv('../data/supermarket_sales.csv')
 
 # convert the list of data to a pandas DataFrame
 data = pd.DataFrame(data)
@@ -12,12 +12,9 @@ data = pd.DataFrame(data)
 data = data.loc[:, ['location', 'lastUpdatedTimestamp', 'incidentLocation', 'stageOfControlCode', 'incidentSizeEstimatedHa']]
 
 
-# make an edit to the data in the Stage of Control column by replacing the values
-data['Stage of Control'] = data['Stage of Control'].replace({'OUT_CNTRL': 'Out Of Control', 'HOLDING': 'Being Held', 'UNDR_CNTRL': 'Under Control'})
-
+'''
 selected_location = 'North Arm Stuart Lake'
 data_location = None
-
 
 def initialize_case_evolution(data, selected_location='China'):
     # Aggregation of the dataframe to erase the regions that will not be used here
@@ -31,13 +28,12 @@ def initialize_case_evolution(data, selected_location='China'):
 
 data_location = initialize_case_evolution(data)
 
-
 def on_change_location(state):
     # state contains all the Gui variables and this is through this state variable that we can update the Gui
     # state.selected_location, state.data_location, ...
     # update data_location with the right location (use initialize_case_evolution)
     print("Chosen location: ", state.selected_location)
-    state.data_location = initialize_case_evolution(data, state.selected_location)
+    state.data_location = initialize_case_evolution(data, state.selected_location)'''
     
 
 dashboard_md = Markdown("pages/dashboard/dashboard.md")
