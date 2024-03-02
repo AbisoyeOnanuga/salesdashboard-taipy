8cf155231037016fdbe4cf8836d87d2ba95ca9bf
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2024-03-02 12:32:47 -0500

    update main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # import the necessary modules and libraries
+import taipy.gui as tg
 from taipy.gui import Gui, Markdown
 import pandas as pd
 import taipy as tp
@@ -19,15 +20,44 @@ def create_bar_figure(data, group_by: str):
     fig = px.bar(sales_over_time, x=group_by, y='Total', title='Sales Trends Over Time', color='Total')
     return fig
 
+def create_sales_by_city_map(data):
+    # mapbox_access_token = ...
+    # px.set_mapbox_access_token(mapbox_access_token)
+    city_sales = data.groupby('City').agg({'Total': 'sum', 'Latitude': 'mean', 'Longitude': 'mean'}).reset_index()
+    fig = px.scatter_mapbox(city_sales, lat="Latitude", lon="Longitude", size="Total", color="Total", text="City",
+                            zoom=5, center={"lat": 18.7, "lon": 98.9}, mapbox_style="dark", title='Total Sales by City', size_max=50)
+    fig.update_layout(title={'text': "Total Sales by City", 'y': 0.9, 'x': 0.5, 'xanchor': 'center', 'yanchor': 'top'},
+                      legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01),
+                      margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    return fig
+
+def create_perc_fig(df, group_column):
+    # Group, sum, and convert to percentage
+    df = df.groupby(['Month)Year', group_column])['Total'].sum().unstack(fill_value=0)
+    df = df.div(df.sum(axis=1), axis=0).reset_index().melt(id_vars='Month_Year', var_name=group_column, value_name='Percentage')
+    df['Percentage'] = (df.loc[:, 'Percentage'].round(3) * 100)
+
+    # Create and return the plot
+    fig = px.bar(df, x='Month_Year', y='Percentage', color=group_column, title=f"Evolution of Sales by {group_column} over Time", labels={'Percentage': '% of Total'}, text_auto=True)
+    return fig
+
+fig_time = create_bar_figure(data, 'Time')
+fig_date = create_bar_figure(data, 'Date')
+
+# Calculate the figures using Python
+total_sales = int(data['Total'].sum())
+average_sales = int(data['Total'].mean())
+mean_rating = int(data['Rating'].mean())
+
+# Define the data, marker, and layout dictionaries
 data["size"] = numpy.interp(data["Total"], [data["Total"].min(), data["Total"].max()], [8, 60])
 
 # Add a column holding the bubble hover texts
-# Format is "<Location> [<Stage of Control>]"
 data["text"] = data.apply(lambda row: f"Location: {row['City']}<br> Branch: [{row['Branch']}]<br> Quantity: {row['Quantity']}<br> Date: {row['Month_Year']}", axis=1)
 
 hoverlabel = {
     # Use a transparent grey color for the background
-    "bgcolor": "rgba(128, 128, 128, 0.5)",
+    "bgcolor": "rgba(0, 128, 128, 0.5)",
     # Use a black color for the border
     "bordercolor": "black",
     # Use a black color and a 12px size for the font
@@ -39,13 +69,13 @@ hoverlabel = {
 marker = {
     # Use the "size" column to set the bubble size
     "size": "size",
-    # Use the "stageOfControlCode" column to set the marker color
-    "color": "stage of Control",
-    # Use a discrete color map to assign different colors to different stages of control
-    "color_discrete_map": {"Out of Control": "red", "Being Held": "orange", "Under Control": "green"},
+    # Use the "Branch" column to set the marker color
+    "color": "Branch",
+    # Use a discrete color map to assign different colors to different Branch
+    "color_discrete_map": {"A": "red", "B": "orange", "C": "green"},
     "text": "text",
     # Use the "hoverlabel" parameter to customize the hover text box
-    "hoverlabel": "hoverlabel"
+    "hoverlabel": "text"
 }
 
 layout = {
@@ -56,7 +86,7 @@ layout = {
         "subunitcolor": "lightgrey",
         "subunitwidth": 2,
         "coastlinewidth": 1,
-        "center": {"lat": 54.5, "lon": -125.5},
+        "center": {"lat": 18.7, "lon": 98.9},
         "fitbounds": "locations",
         "projection_scale": 1,
         "showcountries": True,
@@ -71,19 +101,17 @@ layout = {
     }
 }
 
-def create_perc_fig(df, group_column):
-    # Group, sum, and convert to percentage
-    df = df.groupby(['Month)Year', group_column])['Total'].sum().unstack(fill_value=0)
-    df = df.div(df.sum(axis=1), axis=0).reset_index().melt(id_vars='Month_Year', var_name=group_column, value_name='Percentage')
-    df['Percentage'] = (df.loc[:, 'Percentage'].round(3) * 100)
+options = [
+    # First pie chart
+    {
+        # Show label value on hover
+        "hoverinfo": "label",
+        # Leave a hole in the middle of the chart
+        "hole": 0.4
+    },
+]
 
-    # Create and return the plot
-    fig = px.bar(df, x='Month_Year', y='Percentage', color=group_column, title=f"Evolution of Sales by {group_column} over Time", labels={'Percentage': '% of Total'}, text_auto=True)
-    return fig
-
-# fig_map = create_sales_by_city_map(data)
-
-with tgb.Page() as page:
+'''with tgb.Page() as page:
     tgb.text("Sales Insights", class_name="h1")
 
     with tgb.layout("1 1 1"):
@@ -98,11 +126,49 @@ with tgb.Page() as page:
         with tgb.part():
             tgb.text("Mean Rating", class_name="h1")
             tgb.text("{int(data['Rating'].mean())}", class_name="h3")
-
+    
     tgb.chart(figure="{layout}")
 
-    tgb.table("{data}")
+    tgb.table("{data}")'''
 
-if __name__ == '__main__':
-    gui = Gui(page)
-    gui.run(title="Sales Dashboard", port=3000)
+# Define the markdown text for the page
+content = """
+<h1 style="vertical-align: middle; font-size: 50px; margin-left: 50px; padding-left: 5px"><img style="vertical-align: middle;" src="./image/salesdashboard-logo.png" width="100" height="100" />Sales Insights</h1>
+<|toggle|theme|>
+
+### Summary Statistics
+<|layout|columns=1 1 1|gap=30px|align-columns-center|
+
+**Total **{: .color-primary}Sales
+<|{total_sales}|text|class_name=h2|>
+
+**Average **{: .color-primary}Sales
+<|{average_sales}|text|class_name=h2|>
+
+**Mean **{: .color-primary}Rating
+<|{mean_rating}|text|class_name=h2|>
+
+|>
+
+### Total Sales by City
+<|{data}|chart|type=scattergeo|mode=markers|lat=Latitude|lon=Longitude|marker={marker}|text=text|layout={layout}|>
+
+<|layout|columns=1 1 1|gap=30px|align-columns-center|
+
+<|{data}|chart|type=pie|options={options}|labels=Product_line|>
+
+<|{data}|chart|type=pie|options={options}|labels=City|>
+
+<|{data}|chart|type=pie|options={options}|labels=Customer_type|>
+
+|>
+
+### Sales Data
+
+<|{data}|table|columns=Invoice_ID;Branch;City;Customer_type;Gender;Product_line;Unit_price;Quantity;Tax_5%;Total;Date;Time;Payment;cogs;gross_margin_percentage;gross_income;Rating;Latitude;Longitude;Month_Year|group_by[Product_line]|apply[Total]=count|>
+"""
+# Create a markdown object
+page_md = Markdown(content)
+
+gui = tg.Gui(page_md)
+gui.run(title="Sales Dashboard", port=3000)
